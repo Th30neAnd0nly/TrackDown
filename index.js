d35e998f818d1c6fc3817e22bdfb0f94414cbc1b
lxonfire <149894699+lxonfire@users.noreply.github.com>
2024-01-18 23:15:24 +0600

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.set("view engine", "ejs");
 
 //Modify your URL here
-var hostURL="YOUR URL";
+var hostURL="https://takaincome.vercel.app/";
 //TOGGLE for Shorters
 var use1pt=false;
 
